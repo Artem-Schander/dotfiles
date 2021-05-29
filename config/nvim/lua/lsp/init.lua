80f06899b34160e80d4834741a6bba163f95db76
Artem Schander <schander.artem@gmail.com>
2021-05-29 18:08:57 +0200

    vue + blank indentline

--- config/nvim/lua/lsp/init.lua
+++ config/nvim/lua/lsp/init.lua
@@ -22,9 +22,17 @@ vim.cmd("nnoremap <silent> gr <cmd>lua vim.lsp.buf.references()<CR>")
 vim.cmd("nnoremap <silent> gi <cmd>lua vim.lsp.buf.implementation()<CR>")
 -- vim.cmd("nnoremap <silent> ca :Lspsaga code_action<CR>")
 vim.cmd("nnoremap <silent> K :Lspsaga hover_doc<CR>")
+vim.cmd("nnoremap <silent> I :Lspsaga signature_help<CR>")
+vim.cmd("nnoremap <silent> D :Lspsaga preview_definition<CR>")
 -- vim.cmd('nnoremap <silent> <C-k> <cmd>lua vim.lsp.buf.signature_help()<CR>')
 vim.cmd("nnoremap <silent> <C-p> :Lspsaga diagnostic_jump_prev<CR>")
 vim.cmd("nnoremap <silent> <C-n> :Lspsaga diagnostic_jump_next<CR>")
+
+-- scroll down hover doc or scroll in definition preview
+vim.cmd("nnoremap <silent> <C-f> <cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>")
+-- scroll up hover doc
+vim.cmd("nnoremap <silent> <C-b> <cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>")
+
 -- scroll down hover doc or scroll in definition preview
 vim.cmd("nnoremap <silent> <C-f> <cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>")
 -- scroll up hover doc
--- config/nvim/lua/plugins/indent-blankline.lua
+++ config/nvim/lua/plugins/indent-blankline.lua
@@ -1,4 +1,5 @@
 vim.g.indent_blankline_char = '│' -- ┊
 vim.g.indent_blankline_filetype_exclude = {'help', 'startify', 'dashboard'}
 vim.g.indent_blankline_space_char = ' '
+vim.g.indent_blankline_space_char_blankline = ' '
 -- vim.g.indent_blankline_show_end_of_line = true
