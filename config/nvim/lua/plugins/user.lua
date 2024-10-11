6371451b44a594501f8dc6b1ddd7f25710e1833b
Artem Schander <schander.artem@gmail.com>
2024-10-11 12:07:25 +0200

    add astro nvim dependencies

--- config/nvim/lua/plugins/user.lua
+++ config/nvim/lua/plugins/user.lua
@@ -423,4 +423,9 @@ return {
       }
     end
   },
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
+    config = true,
+  },
 }
