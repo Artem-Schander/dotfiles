1f3d37b8194f63919431c21a44711c5f5ed740b1
Artem Schander <schander.artem@gmail.com>
2025-01-17 10:39:11 +0100

    update

--- config/nvim/lua/plugins/user.lua
+++ config/nvim/lua/plugins/user.lua
@@ -406,9 +406,9 @@ return {
       }
     end
   },
-  {
-    "vhyrro/luarocks.nvim",
-    priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
-    config = true,
-  },
+  -- {
+  --   "vhyrro/luarocks.nvim",
+  --   priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
+  --   config = true,
+  -- },
 }
